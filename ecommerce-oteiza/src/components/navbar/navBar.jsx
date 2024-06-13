d73da75fb2fb1aee34d3737fb96bb8266393d71f
foteiza <fede.oteiza@gmail.com>
2024-06-13 12:55:08 -0300

    Agregando carpetas y archivos

---
+++ ecommerce-oteiza/src/components/navbar/navBar.jsx
@@ -0,0 +1,9 @@
+import "./navBarStyles.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbarBackground">
+        <h1 className="navbarFontColor">Probando navbar</h1>
+    </div>
+  )
+}
