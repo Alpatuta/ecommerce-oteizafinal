bba2f2272a824cc6b855f03fb103f2008cef2b56
foteiza <fede.oteiza@gmail.com>
2024-06-25 18:35:52 -0300

    Agregando el archivo productos

---
+++ ecommerce-oteiza/src/productos.js
@@ -0,0 +1,42 @@
+export let products = [
+  {
+    id: 1,
+    title: "Whey protein star nutrition 3kg",
+    price: 5900,
+    description:
+      "Whey Protein de Star Nutrition: Suplemento de alta calidad para crecimiento muscular y recuperación eficiente.",
+    img: "https://res.cloudinary.com/dfa3ts2kx/image/upload/v1718748379/PLATINUM_20WHEY_20PROTEIN_20-_20Cookies_20And_20Cream_20-_203_20KG_tx5eou.png",
+    stock: 10,
+    category: "proteinas",
+  },
+  {
+    id: 2,
+    title: "Creatina star nutrition 300g",
+    price: 2000,
+    stock: 5,
+    description:
+      "La creatina de Star Nutrition mejora el rendimiento físico, incrementa la fuerza y acelera la recuperación muscular.",
+    category: "creatinas",
+    img: "https://res.cloudinary.com/dfa3ts2kx/image/upload/v1718748486/thumb.php_buelxl.png",
+  },
+  {
+    id: 3,
+    title: "Pre-entreno star nutrition 300g",
+    price: 2500,
+    stock: 5,
+    description:
+      "Pre-entreno Star Nutrition: Aumenta tu energía, resistencia y enfoque con ingredientes de alta calidad y rápida absorción.",
+    category: "pre-entrenos",
+    img: "https://res.cloudinary.com/dfa3ts2kx/image/upload/v1718748560/pump_v8_vbjovh.webp",
+  },
+  {
+    id: 4,
+    title: "Creatina cibeles 350g",
+    price: 2400,
+    stock: 5,
+    description:
+      "La creatina de Cibeles mejora la fuerza y el rendimiento muscular, ideal para entrenamientos intensos.",
+    category: "creatinas",
+    img: "https://res.cloudinary.com/dfa3ts2kx/image/upload/v1719350935/cn-creatina_150-removebg-preview_ncnrvu.jpg",
+  },
+];
