a98d0f167d8c2976d34829c24e993a13637bff65
foteiza <fede.oteiza@gmail.com>
2024-07-27 18:34:47 -0300

    Poniendome al dia pt1

--- ecommerce-oteiza/src/page/itemDetailContainer/ItemDetail.jsx
+++ ecommerce-oteiza/src/page/itemDetailContainer/ItemDetail.jsx
@@ -1,12 +1,33 @@
 import CounterContainer from "../../components/counter/CounterContainer";
+import "./itemDetailStyles.css";
 
-const ItemDetail = ({ item }) => {
+const ItemDetail = ({ item, onAdd }) => {
   return (
-    <div style={{ color: "white" }}>
-      <h2>{item.title}</h2>
-      <h3>{item.price}</h3>
-      <CounterContainer />
-    </div>
+    <>
+      <div>
+        <div className={"containerItemDetail"}>
+          <div className={"containerImage"}>
+            <img src={item.img} alt="" />
+          </div>
+
+          <div className={"containerDetail"}>
+            <h2 style={{ fontFamily: "Poppins" }}>
+              <span style={{ fontSize: "23px" }}>Nombre:</span>{" "}
+              <h4 className="texto-detail">{item.title}</h4>
+            </h2>
+            <h2 style={{ fontFamily: "Poppins" }}>
+              <span style={{ fontSize: "23px" }}>Descripcion:</span>{" "}
+              <h4 className="texto-detail"> {item.description}</h4>
+            </h2>
+            <h2 style={{ fontFamily: "Poppins" }}>
+              <span style={{ fontSize: "23px" }}>Precio:</span>{" "}
+              <h5 className="texto-detail">${item.price}.</h5>
+            </h2>
+          </div>
+        </div>
+        <CounterContainer onAdd={onAdd} />
+      </div>
+    </>
   );
 };
 
